c46c44c543ad1c6d9105561f86ba0b50d5c0d9c2
StyrofoamEater123 <sputnikmarbles@proton.me>
2025-10-26 10:17:46 -0400

    mod id

---
+++ src/main/java/com/marbles/paracraft/Paracraft.java
@@ -0,0 +1,31 @@
+package com.marbles.paracraft;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Paracraft.MODID, name = Paracraft.NAME, version = Paracraft.VERSION)
+public class Paracraft
+{
+    public static final String MODID = "paracraft";
+    public static final String NAME = "Paracraft Core";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
